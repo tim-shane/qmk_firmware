2cd683e7a2d10f9ac854580b99b2c71aecefb2eb
tim <tim.edwards@datawalk.com>
2022-08-08 11:22:01 -0500

    initial keyboard keymap

---
+++ keyboards/splitkb/kyria/keymaps/prime/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+CAPS_WORD_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+MOUSEKEY_ENABLE = yes
